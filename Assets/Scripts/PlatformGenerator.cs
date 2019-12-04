92448ba078d22c929e75a129b07e1cc0efd46a37
Antonio Stankovic <antoniostankovic123@gmail.com>
2019-12-04 13:40:24 +0100

    generate platform and camera follow

---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+    public GameObject platform;
+    public Transform generationPoint;
+
+    private float platformWidth;
+
+    void Start() {
+        platformWidth = platform.GetComponentInChildren<BoxCollider>().size.z;
+    }
+
+    void Update() {
+
+        if (transform.position.z < generationPoint.transform.position.z) {
+            transform.position = new Vector3(transform.position.x, transform.position.y,  transform.position.z + platformWidth);
+            Instantiate(platform, transform.position, transform.rotation);
+        }
+    }
+}
